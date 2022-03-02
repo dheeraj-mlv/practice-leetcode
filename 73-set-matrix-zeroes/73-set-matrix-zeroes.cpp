c7dc8aa67a5a158f9bd84d44d28ba454af3b333a
dheeraj-mlv <73815505+dheeraj-mlv@users.noreply.github.com>
2022-03-02 20:42:57 +0530

    Time: 30 ms (20.27%), Space: 13.2 MB (82.13%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,60 +1,86 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
+    void setZeroes(vector<vector<int>>& matrix){
 
-        int r,c;
+        // no of rows and no of columns
 
-         r=matrix.size();
+        int r=matrix.size();
+        int c=matrix[0].size();
 
-         c=matrix[0].size();
+        // 2 pointers for marking the reference row
 
-        // unordered map or set to store the values and traverse at the end 
+        bool row=false,col=false;
 
-        unordered_map<int,int> row;
-        unordered_map<int,int> col;
+        // traverse through the matrix and mark the reference row elements
 
         for (int i = 0; i < r; i++)
         {
             for (int j = 0; j <c; j++)
             {
                 if(matrix[i][j]==0){
-                    row[i]++;
-                    col[j]++;
+                    // belongs to first row then use the markers place
+                    if(i==0){
+                        row=true;
+                    }
+                    // else directly mark the corresponding reference row elements
+                    else{
+                        matrix[i][0]=0;
+                    }
+                    // 
+                    if(j==0){
+                        col=true;
+                    }
+                    // 
+                    else{
+                          matrix[0][j]=0;
+                    }
                 }
+                
             }
             
         }
-        
-        for(auto x:row){
-            // make all elements of the row zero
 
-            int row_number=x.first;
+        for (int i = 1; i < c; i++)
+        {
+            if(matrix[0][i]==0){
+                for (int j = 0; j < r; j++)
+                {
+                    matrix[j][i]=0;
+                }
+                
+            }
 
+        }
+
+        for (int i = 1; i < r; i++)
+        {
+            if(matrix[i][0]==0){
+                for (int j = 0; j < c; j++)
+                {
+                    matrix[i][j]=0;
+                }
+                
+            }
+
+        }
+
+        if(row==true){
             for (int i = 0; i < c; i++)
             {
-                matrix[row_number][i]=0;
+                matrix[0][i]=0;
             }
- 
+            
         }
-
-        for(auto x:col){
-            int col_number=x.first;
+        if(col==true){
             for (int i = 0; i < r; i++)
             {
-                matrix[i][col_number]=0;
+                matrix[i][0]=0;
             }
             
         }
 
-
-
-
-
-
-
-
-
-       
 
     }
 };
+
+
