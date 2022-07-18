f389ee317ea87d1f7e1ab774dc1d8f60914df543
dheeraj-mlv <73815505+dheeraj-mlv@users.noreply.github.com>
2022-07-19 01:05:25 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (19.19%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     int f(int n,vector<int> &dp){
+//         need to do nothing for the base cases
         if(n==0) return 0;
         if(n==1) return 1;
         if(n==2) return 1;
-        
+//         for the remaining cases just check if it is already caluculated or not 
+//         if it is caluculated just return the found value
         if(dp[n]!=-1) return dp[n];
+//         caluculate and store the value
         return dp[n]=f(n-1,dp)+f(n-2,dp)+f(n-3,dp);
         
     }
