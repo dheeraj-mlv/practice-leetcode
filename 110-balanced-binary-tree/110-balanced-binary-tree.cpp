d0f5cbc0f500309416081d37acb72a533cd2b0ce
dheeraj-mlv <73815505+dheeraj-mlv@users.noreply.github.com>
2022-07-20 15:10:34 +0530

    Time: 25 ms (33.23%), Space: 21 MB (54.12%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -11,14 +11,17 @@
  */
 class Solution {
 public:
+//     the unbalenced nature of the tree is indicated by -1 and balenced nature is done by anyvalue between 0-height
     int height(TreeNode *root){
+// using the normal height function and just make it return -1 if there is a unbalenced binary tree 
         if(root==NULL) return 0;
-        
+//         find the left height and right height 
         int lh=height(root->left);
         int rh=height(root->right);
-        
+//         if anyone of them is -1 then the corresponding tree is unbalenced
         if(lh==-1||rh==-1) return -1;
         
+//         check if the abs difference in the heights if it  is greater than 1 return unbalenced nature 
         if(abs(lh-rh)>1) return -1;
         
         return 1+max(lh,rh);
