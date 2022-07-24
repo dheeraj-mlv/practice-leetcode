759c09888f1f8c9504abd17f0048449d0b86748b
dheeraj-mlv <73815505+dheeraj-mlv@users.noreply.github.com>
2022-07-24 17:54:48 +0530

    Time: 18 ms (78.49%), Space: 16.3 MB (57.11%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -1,4 +1,12 @@
 #define f(i,l,r) for(int i=l;i<r;i++)
+// Naive solution is to create a new array and put numbers in it 
+// Now I just need a way where I can store 2 things in the same place so I will make the array as follows
+// access the numbers at the nums[nums[i]] and multiply it by n and add to this number when modulated by n we will get the original number 
+// when divided by n we will get the new number
+
+// example A=A'+kx where A'<k  
+// A/k= x
+// A%k=A'
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
