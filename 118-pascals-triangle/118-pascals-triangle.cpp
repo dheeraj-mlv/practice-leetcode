9e4387a191fafb35b9a3675a6a20ed60870291cf
dheeraj-mlv <73815505+dheeraj-mlv@users.noreply.github.com>
2022-09-01 18:42:53 +0530

    Time: 7 ms (5.41%), Space: 6.5 MB (32.59%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,71 +1,15 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-
-        int n=numRows;
-
-        vector<int> v;
-        v.push_back(1);
-        
+    vector<vector<int>> generate(int n) {
         vector<vector<int>> ans;
-        ans.push_back({1});
-
-        cout<<1<<endl;
-
-        for (int i = 1; i < n; i++)
-        {
-           vector<int> temp;
-           temp.push_back(1);
-
-           for (int i = 1; i < v.size(); i++)
-           {
-               temp.push_back(v[i]+v[i-1]);
-           }
-
-           temp.push_back(1);
-
-
-           for(auto x:v){
-               v.pop_back();
-           }
-
-
-           for(auto x:temp){
-               v.push_back(x);
-           }
-
-
-           for(auto x:v){
-               cout<<x<<" ";
-           }
-           cout<<endl;
-            
+        for(int i=0;i<n;i++){
+            vector<int> temp;
+            for(int j=0;j<=i;j++){
+                if(j==0 || j==i) temp.push_back(1);
+                else temp.push_back(ans[i-1][j-1]+ans[i-1][j]);
+            }
             ans.push_back(temp);
-
-
-           for(auto x:temp){
-               temp.pop_back();
-           }
         }
-        
-
-
-
-        
-
-
-
-
- 
-        
-
-
         return ans;
     }
 };
-
-
-
-
-
-
